a8de8421b6a89d078e6668967f1a09beb6222a1c
ahnl <46541386+ahnl@users.noreply.github.com>
2021-05-03 13:18:16 +0300

    Rephrasing

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
                 <Header />
                 <Portfolio data={portfolioData} />
                 <div className="centerText">
-                    You may contact me for my full CV, with a more detailed work history. References upon a request.
+                    You may contact me for my full CV, with a more detailed work history. References available upon request.
                     <br /><SocialMedias />
                 </div>
             </div>
